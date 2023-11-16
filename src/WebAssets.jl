2efd787caf9ab859f317afbb36f09a666d85ea4c
Josh Day <emailjoshday@gmail.com>
2023-11-16 07:12:38 -0500

    wip

--- src/WebAssets.jl
+++ src/WebAssets.jl
@@ -15,12 +15,6 @@ url2filename(url) = replace(lowercase(url), charmap...)
 
 filename2url(file) = replace(file, reverse.(charmap)...)
 
-#-----------------------------------------------------------------------------# VersionedAsset
-struct VersionedAsset
-    get_versions::Function
-    get_url::Function
-end
-
 #-----------------------------------------------------------------------------# @asset
 macro asset(url)
     Base.remove_linenums!(esc(quote
@@ -45,12 +39,5 @@ macro list(pkg)
         end
     end)
 end
-
-#-----------------------------------------------------------------------------#
-# function github_latest_release(owner::AbstractString, repo::AbstractString; key::Symbol = :name)
-#     url = "https://api.github.com/repos/$owner/$repo/releases/latest"
-#     obj = JSON3.read(read(download(url)))
-#     VersionNumber(obj[key])
-# end
 
 end
