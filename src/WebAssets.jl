330b7f688eeaab2d2a7a8419b1dd4606aaa2cd19
Josh Day <emailjoshday@gmail.com>
2023-11-24 10:29:02 -0500

    make macro depend on function.  Clean up tests

--- src/WebAssets.jl
+++ src/WebAssets.jl
@@ -6,7 +6,8 @@ import Downloads: download
 export @asset, @list
 
 #-----------------------------------------------------------------------------# settings
-scratchdir = "__web_assets__"
+"Subdirectory of the scratch directory to store assets in."
+scratchdir = "WebAssets_jl"
 
 #-----------------------------------------------------------------------------# url2filename
 charmap = [':' => 'C', '/' => 'S', '?' => 'Q']
@@ -15,22 +16,47 @@ url2filename(url) = replace(lowercase(url), charmap...)
 
 filename2url(file) = replace(file, reverse.(charmap)...)
 
-#-----------------------------------------------------------------------------# @asset
-macro asset(url)
-    Base.remove_linenums!(esc(quote
-        let
-            dir = WebAssets.Scratch.get_scratch!($__module__, WebAssets.scratchdir)
-            path = joinpath(dir, WebAssets.url2filename($url))
-            isfile(path) || WebAssets.download($url, path)
-            path
-        end
-    end))
+
+#-----------------------------------------------------------------------------# macros
+function asset(mod::Module, url::AbstractString)
+    dir = Scratch.get_scratch!(mod, scratchdir)
+    path = joinpath(dir, url2filename(url))
+    try
+        isfile(path) || WebAssets.download(url, path)
+    catch
+        error("`@asset \"$url\"` failed to download.  Check the URL and your internet connection.")
+    end
+    return path
 end
 
+asset(mod::Module, f::Function) = asset(mod, f())
+
+
+"""
+    @asset url
+
+Download the asset at `url` and return the path to the downloaded file.
+If the asset has already been downloaded, the cached version will be returned.
+"""
+macro asset(url)
+    esc(:(WebAssets.asset($__module__, $url)))
+end
+
+
+"""
+    @list
+
+Return a list of all assets (downloaded via the `@asset` macro) of the current module.
+"""
 macro list()
     esc(:(WebAssets.@list($__module__)))
 end
 
+"""
+    @list pkg
+
+Return a list of all assets (downloaded via the `@asset` macro) of the module `pkg`.
+"""
 macro list(pkg)
     esc(quote
         let
--- test/runtests.jl
+++ test/runtests.jl
@@ -5,37 +5,32 @@ using Scratch
 using WebAssets
 
 with_scratch_directory(mkpath(joinpath(tempdir(), "WebAssetsTests"))) do
-    clear_scratchspaces!()
+    @testset "Tests" begin
+        # verify that the scratchspace is empty
+        clear_scratchspaces!()
+        dir = get_scratch!(".")
+        @test isempty(readdir(dir))
 
-    # start with blank slate
-    dir = get_scratch!(".")
-    @test isempty(readdir(dir))
+        # add asset and test that it exists in the scratchspace
+        plotly = @asset "https://cdn.plot.ly/plotly-2.24.0.min.js"
+        @test @allocated(@asset "https://cdn.plot.ly/plotly-2.24.0.min.js") < 15_000 # verify nothing is downloaded (allocations due to Scratch.jl)
+        @test !isempty(readdir(dir))
+        @test isfile(plotly)
+        @test length(WebAssets.@list()) == 1
 
-    # add asset and test that it exists in the scratchspace
-    url = "https://cdn.plot.ly/plotly-2.24.0.min.js"
-    plotly = @asset url
-    @test !isempty(readdir(dir))
-    @test isfile(plotly)
-    @test length(WebAssets.@list()) == 1
+        # add same asset with url variable
+        url = "https://cdn.plot.ly/plotly-2.24.0.min.js"
+        plotly2 = @asset url
+        @test plotly == plotly2
+        @test isfile(plotly2)
+        @test length(WebAssets.@list()) == 1
 
-    # Test that file isn't downloaded again
-    @test @allocated(plotly2 = @asset url) < 15_000
-
-    clear_scratchspaces!()
-    # test that string interpolation works
-    v = "2.24.0"
-    url = "https://cdn.plot.ly/plotly-$v.min.js"
-    plotly = @asset url
-    @test !isempty(readdir(dir))
-    @test isfile(plotly)
-    @test length(WebAssets.@list()) == 1
-
-    @test @allocated(plotly2 = @asset url) < 15_000
-
-    # cleanup
-    clear_scratchspaces!()
-    @test !isfile(plotly)
-    @test length(WebAssets.@list()) == 0
+        # different asset with string interpolation
+        v = "2.27.1"
+        plotly3 = @asset "https://cdn.plot.ly/plotly-$v.min.js"
+        @test isfile(plotly3)
+        @test length(WebAssets.@list()) == 2
+    end
 end
 
 end #module
