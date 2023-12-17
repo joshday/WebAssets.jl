67ed6c56b1635e6ae963ad13c47e619603dfbbe6
Josh Day <emailjoshday@gmail.com>
2023-12-17 11:23:28 -0600

    tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,23 +5,24 @@ using Scratch
 using WebAssets
 using WebAssets: add!, remove!, list, update!, dir!, url2filename, filename2url
 
-url = "https://cdn.plot.ly/plotly-2.24.0.min.js"
+url = "HTTPS://cdn.plot.ly/plotly-2.24.0.min.js"
 
 file = url2filename(url)
 @test file == "httpsCSScdn.plot.lySplotly-2.24.0.min.js"
-@test filename2url(file) == url
+@test filename2url(file) == lowercase(url)
 
 
 path = mkpath(joinpath(tempdir(), "WebAssets_jl_tests"))
 
 dir!(path)
+foreach(remove!, list())
 
 @test WebAssets.DIR[] == path
 @test list() == []
 
 add!(url)
 
-@test list() == [url]
+@test list() == [lowercase(url)]
 
 update!()
 update!(url)
